3d0350aa8a083b592f4024d061cd0c768d76b98c
june <108857031+juneeeee13@users.noreply.github.com>
2024-11-19 10:51:44 -0800

    Update createtables.sql

--- backend/createtables.sql
+++ backend/createtables.sql
@@ -1,36 +1,41 @@
 CREATE TABLE users (
-	user_id INT Primary Key NOT NULL,
-	username VARCHAR(20) NOT NULL,
-	password VARCHAR(15) NOT NULL,
-	age INT NOT NULL,
-	email VARCHAR(254) NOT NULL,
-	isAdmin BOOL,
-	isBlacklisted BOOL,
-	profileImg VARCHAR
+    user_id INT PRIMARY KEY NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    isAdmin BOOLEAN DEFAULT FALSE,
+    isBlacklisted BOOLEAN DEFAULT FALSE,
+    profileImg VARCHAR
 );
 
 CREATE TABLE posts (
-	post_id INT Primary Key NOT NULL,
-	title VARCHAR(20) NOT NULL,
-	content VARCHAR(300) NOT NULL,
-	images VARCHAR,
-	created_at TIMESTAMP,
-	like_no INT,
-	hashtags VARCHAR,
-	category VARCHAR
+    post_id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    content VARCHAR(300) NOT NULL,
+    images VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    like_no INT DEFAULT 0,
+    hashtags VARCHAR,
+    category VARCHAR
 );
 
 CREATE TABLE reports (
-	report_id INT Primary Key NOT NULL,
-	reporter_id INT Foreign Key NOT NULL,
-	user_reported INT Foreign Key NOT NULL,
-	post_id INT Foreign Key NOT NULL
+    report_id INT PRIMARY KEY NOT NULL,
+    reporter_id INT NOT NULL,
+    user_reported INT NOT NULL,
+    post_id INT NOT NULL,
+    FOREIGN KEY (reporter_id) REFERENCES users(user_id),
+    FOREIGN KEY (user_reported) REFERENCES users(user_id),
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
 CREATE TABLE replies (
-	reply_id INT Primary Key NOT NULL,
-	post_id INT Foreign Key,
-	content VARCHAR(300) NOT NULL,
-	image VARCHAR NOT NULL,
-	user_id INT Foreign Key
+    reply_id INT PRIMARY KEY NOT NULL,
+    post_id INT,
+    content VARCHAR(300) NOT NULL,
+    image VARCHAR,
+    user_id INT,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
