5c64b89e1d2457a46ec61bd93916e3f91b9b82df
june <108857031+juneeeee13@users.noreply.github.com>
2024-11-19 12:07:13 -0800

    Create insertusers.sql

---
+++ backend/insertusers.sql
@@ -0,0 +1,5 @@
+INSERT users (user_id, username, password, age, email, isAdmin, isBlacklisted, profileImg)
+  VALUES
+  (1, 'hithisisjune', '11223344', 20, 'phill203@csusm.edu', 1, 0, '../../esdeeimgs/girlpfp.jpg'),
+  (2, 'goku9000', 'supersaiyan', 19, 'validemail@gmail.com', 0, 0, '../../esdeeimgs/girlpfp2.jpg'),
+  (3, 'randomgirl', 'hiiii', 22, 'validemail@outlook.com', 0, 0, '../../esdeeimgs/girlpfp3.jpg')
