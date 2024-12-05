ad924c342e67f8e7b1b690e498380d176f5d758c
Takehiko <127874040+Franky0112@users.noreply.github.com>
2024-12-04 16:13:59 -0800

    Update createtables.sql

--- backend/createtables.sql
+++ backend/createtables.sql
@@ -11,16 +11,19 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
     post_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    poster_id INT NOT NULL,
+    -- poster_id INT NOT NULL,
+    user_id INT NOT NULL,
     title VARCHAR(20) NOT NULL,
     content VARCHAR(300) NOT NULL,
-    images VARCHAR(255),
+    post_image LONGBLOB,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     like_no INT DEFAULT 0,
     hashtags VARCHAR(255),
     category VARCHAR(255),
-    FOREIGN KEY (poster_id) REFERENCES users(user_id)
+    -- FOREIGN KEY (poster_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
 
 CREATE TABLE reports (
     report_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -36,11 +39,73 @@ CREATE TABLE replies (
     reply_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     post_id INT,
     content VARCHAR(300) NOT NULL,
-    image VARCHAR(255),
+    reply_image LONGBLOB,
     user_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(post_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- CREATE TABLE users (
+--     user_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+--     username VARCHAR(20) NOT NULL,
+--     password VARCHAR(255) NOT NULL,
+--     age INT NOT NULL,
+--     email VARCHAR(254) NOT NULL,
+--     isAdmin TINYINT(1) DEFAULT 0,
+--     isBlacklisted TINYINT(1) DEFAULT 0,
+--     profileImg VARCHAR(255)
+-- );
+
+-- CREATE TABLE posts (
+--     post_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+--     poster_id INT NOT NULL,
+--     title VARCHAR(20) NOT NULL,
+--     content VARCHAR(300) NOT NULL,
+--     images VARCHAR(255),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     like_no INT DEFAULT 0,
+--     hashtags VARCHAR(255),
+--     category VARCHAR(255),
+--     FOREIGN KEY (poster_id) REFERENCES users(user_id)
+-- );
+
+-- CREATE TABLE reports (
+--     report_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+--     reporter_id INT NOT NULL,
+--     user_reported INT NOT NULL,
+--     post_id INT NOT NULL,
+--     FOREIGN KEY (reporter_id) REFERENCES users(user_id),
+--     FOREIGN KEY (user_reported) REFERENCES users(user_id),
+--     FOREIGN KEY (post_id) REFERENCES posts(post_id)
+-- );
+
+-- CREATE TABLE replies (
+--     reply_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+--     post_id INT,
+--     content VARCHAR(300) NOT NULL,
+--     image VARCHAR(255),
+--     user_id INT,
+--     FOREIGN KEY (post_id) REFERENCES posts(post_id),
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
 
 
 
